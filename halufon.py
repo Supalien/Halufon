08ad1e403deb7b7e11790fe4ef6423709b57dbb4
Doron <dorons900@gmail.com>
2019-04-20 14:44:54 +0300

    Random hilufs and cleaner code

--- halufon.py
+++ halufon.py
@@ -25,21 +25,24 @@ async def on_message(message):
     if message.content == f"{client.user.mention} לךלישון" and message.author.id == 220946685612785664:
         await client.close()
 
-    if message.content.startswith(client.user.mention):
+    if message.content == f"{client.user.mention} !אקראי":
+        hiluf = utils.get_random_hiluf()
+        word = hiluf["word"]
+        definition = utils.define(utils.denikud(word))
+        emb = utils.embedify(hiluf, definition)
+        await message.channel.send(f":חילוף של {word}", embed=emb)
+
+    elif message.content.startswith(client.user.mention):
         laaz_word = message.content.replace(client.user.mention + " ", "")
         hiluf = utils.get_hiluf(laaz_word)
-        if hiluf:
-            word_hebrew = hiluf["word_hebrew"]
-            explanation = hiluf["explanation"]
-            emb = discord.Embed(title=word_hebrew, color=random.randint(0,16777215))
-            if hiluf["explanation"]:
-                emb.add_field(name = f":הסבר", value = explanation)
-            definition = utils.define(word_hebrew)
-            if definition:
-                emb.add_field(name = f":משמעות {word_hebrew} ממילוג", value = definition)
-            await message.channel.send(f":חילוף של {laaz_word}", embed=emb)
-        else:
-            await message.channel.send(f"{laaz_word} לא נמצא")
+        if not hiluf:
+            await message.channel.send(f"{laaz_word} .לא נמצא")
+            return
+        definition = utils.define(laaz_word)
+        emb = utils.embedify(hiluf, definition)
+        word = hiluf["word"]
+        await message.channel.send(f":חילוף של {word}", embed=emb)
+
 
 with open("Token.txt") as f:
     token = f.read()
--- utils.py
+++ utils.py
@@ -1,14 +1,22 @@
 import requests as req
 import pickle
 from bs4 import BeautifulSoup
+import random
+import discord
 
 
 def get_hiluf(laaz_word):
     with open("words.pkl", "rb") as f:
         words = pickle.load(f)
     for word in words["words"]:
-        if laaz_word == denikud(word["word"]):
+        if denikud(laaz_word) == denikud(word["word"]):
             return word
+
+
+def get_random_hiluf():
+    with open("words.pkl", "rb") as f:
+        words = pickle.load(f)
+        return random.choice(words["words"])
 
 
 async def update():
@@ -34,3 +42,15 @@ def define(word):
     if not element:
         return
     return element.find("div", {"class": "sr_e_txt"}).text
+
+
+def embedify(hiluf, definition):
+    laaz_word = hiluf["word"]
+    word_hebrew = hiluf["word_hebrew"]
+    explanation = hiluf["explanation"]
+    emb = discord.Embed(title=word_hebrew, color=random.randint(0, 16777215))
+    if explanation:
+        emb.add_field(name=f":הסבר", value=explanation)
+    if definition:
+        emb.add_field(name=f":משמעות {word_hebrew} ממילוג", value=definition)
+    return emb
